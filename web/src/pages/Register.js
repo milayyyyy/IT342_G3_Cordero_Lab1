4cede34f7b8e9018072314bfdd96bb9f58898409
LAB13_USER ON WS21 <L13Y12W21@LabHCI.net>
2026-02-07 12:48:01 +0800

    fix: Remove unnecessary escape characters in regex

--- web/src/pages/Register.js
+++ web/src/pages/Register.js
@@ -24,7 +24,7 @@ const Register = () => {
       uppercase: /[A-Z]/.test(password),
       lowercase: /[a-z]/.test(password),
       digit: /\d/.test(password),
-      special: /[!@#$%^&*()_+\-=\[\]{};:,.<>?]/.test(password),
+      special: /[!@#$%^&*()_+\-=[\]{};:,.<>?]/.test(password),
     };
     setPasswordStrength(strength);
     return Object.values(strength).every(v => v);
